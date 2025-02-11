426b462ab975d9894986ee00738a4c5b7ab04a6b
jovanj91 <jovan.josafat@gmail.com>
2025-02-11 13:43:45 +0700

    update

--- launch/ultrasonic.launch.py
+++ launch/ultrasonic.launch.py
@@ -9,7 +9,7 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-port0',
-                'baud_rate': 115200
+                'baud_rate': 9600
             }]
         )
     ])
