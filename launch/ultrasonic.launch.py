6c397ad25bfa787a20748e6f0151700db1548c32
jovanj91 <jovan.josafat@gmail.com>
2025-02-12 15:35:57 +0700

    update

--- launch/ultrasonic.launch.py
+++ launch/ultrasonic.launch.py
@@ -5,7 +5,7 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='vedita_bot_addon',
-            executable='serial_publisher',
+            executable='us_serial_publisher',
             output='screen',
             parameters=[{
                 'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.2:1.0-port0',
