0c1bb77535c6e1eed13cc66ab6220614e428bf38
jovanj91 <jovan.josafat@gmail.com>
2025-03-04 18:27:25 +0700

    update

--- launch/camera.launch.py
+++ launch/camera.launch.py
@@ -12,9 +12,9 @@ def generate_launch_description():
             namespace='camera',
             parameters=[{
                 'image_size': [640,480],
-                #'time_per_frame': [1, 6],
+                'time_per_frame': [1, 6],
                 'camera_frame_id': 'camera_link_optical',
-                'output_encoding': 'yuv422_yuy2'
+                # 'output_encoding': 'yuv422_yuy2'
             }]
         )
     ])
